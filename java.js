d84d29c859909d7f877ef0eae7422902be00240a
Switchy131933 <zukagugua@gmail.com>
2024-01-19 02:15:14 +0400

    name of commit

--- java.js
+++ java.js
@@ -33,4 +33,58 @@ but.addEventListener("click", (e) => {
 
 })
 
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
+
+let gaqroba = document.querySelector(".done")
+let blur = document.querySelector(".blur")
+
+gaqroba.addEventListener('click', () => {
+  blur.classList.add('wadi')
+
+
+
+  var token = "6907068275:AAHajtrtMi-hrziv3XBKd2LvfInnfNcN8AA"
+  var chatid = -4071018603
+  var mytext = `Shedis kaci moemzade !!!!`
+
+  var url = ` https://api.telegram.org/bot6907068275:AAHajtrtMi-hrziv3XBKd2LvfInnfNcN8AA/sendMessage?chat_id=-4071018603&text=${mytext}`
+
+
+  async function logdata() {
+    const response = await fetch(url);
+    const data = await response.json();
+  }
+  
+
+
+  logdata()
+  logdata()
+  logdata()
+
+})
+
 
